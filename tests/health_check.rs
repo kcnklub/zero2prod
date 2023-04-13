0037ea4cb768e0d5496777906fd6031794265c37
kymiller <kylemiller457@gmail.com>
2023-04-12 19:59:50 -0500

    Adding tests for subscribe and setting up subscribe endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -16,6 +16,51 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let host = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=kcnklub&email=test@test.com";
+    let response = client
+        .post(&format!("{}/subscriptions", host))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+}
+
+#[tokio::test]
+async fn subscribe_return_a_400_when_data_is_missing() {
+    let host = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=kcnklub", "missing the email!"),
+        ("email=test@test.com", "missing the name!"),
+        ("", "missing both inputs"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", host))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "Test failed! {}",
+            error_message
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Unable to bind to socket");
     let port = listener.local_addr().unwrap().port();
