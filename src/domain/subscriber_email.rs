98b75f579c4287748629536c9f73ede78d4dc63e
kymiller <kylemiller457@gmail.com>
2023-05-10 19:40:39 -0500

    Finishing syntactic email validation

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -8,7 +8,7 @@ impl SubscriberEmail {
         if validate_email(&input) {
             Ok(Self(input))
         } else {
-            Err(format!("{} is not a valid email!", input))
+            Err(format!("{input} is not a valid email!"))
         }
     }
 }
@@ -25,6 +25,16 @@ mod tests {
     use fake::{faker::internet::en::SafeEmail, Fake};
 
     use super::SubscriberEmail;
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -44,9 +54,8 @@ mod tests {
         assert_err!(SubscriberEmail::parse(input));
     }
 
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        claims::assert_ok!(SubscriberEmail::parse(email));
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
